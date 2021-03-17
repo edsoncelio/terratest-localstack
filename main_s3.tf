040197b006c9485b959a57ba31fbfaba3b706606
Edson (aka tuxpilgrim) <edsoncelio@users.noreply.github.com>
2021-03-17 10:00:16 -0300

    Update main_s3.tf

--- main_s3.tf
+++ main_s3.tf
@@ -10,7 +10,6 @@ terraform {
 provider "aws" {
   region = "us-east-1"
   s3_force_path_style         = true
-  secret_key                  = "test"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
