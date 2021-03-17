032522020bfbc65bd7ec694f22fafc0d96676bd0
Edson (aka tuxpilgrim) <edsoncelio@users.noreply.github.com>
2021-03-17 20:54:05 -0300

    Update main_s3.tf

--- main_s3.tf
+++ main_s3.tf
@@ -20,7 +20,7 @@ provider "aws" {
 }
 
 module "s3_bucket" {
-  source = "git@github.com:marcelomansur/iac-modulo-s3.git?ref=testing"
+  source = "github.com:marcelomansur/iac-modulo-s3.git?ref=testing"
   bucket_name = var.bucket_name
   acl = var.acl
 }
