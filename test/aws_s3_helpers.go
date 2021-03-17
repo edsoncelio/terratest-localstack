7137494cf99d7c66642b61c00f24a69ea34e432e
Edson (aka tuxpilgrim) <edsoncelio@users.noreply.github.com>
2021-03-17 10:17:10 -0300

    Update aws_s3_helpers.go

--- test/aws_s3_helpers.go
+++ test/aws_s3_helpers.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/aws"
+	//"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
@@ -14,41 +14,31 @@ import (
 func runAwsS3Localstack(t *testing.T) {
 	t.Parallel()
 
-	// Give this S3 Bucket a unique ID for a name tag so we can distinguish it from any other Buckets provisioned
-	// in your AWS account
 	expectedName := fmt.Sprintf("s3-bucket-localstack-%s", strings.ToLower(random.UniqueId()))
 
 	// Pick a random AWS region to test in. This helps ensure your code works in all regions.
-	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
+	//awsRegion := aws.GetRandomStableRegion(t, nil, nil)
 
-	// Construct the terraform options with default retryable errors to handle the most common retryable errors in
-	// terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		// The path to where our Terraform code is located
 		TerraformDir: "../",
 
-		// // Variables to pass to our Terraform code using -var options
 		Vars: map[string]interface{}{
 			"bucket_name":        expectedName,
 		},
 
-		// Environment variables to set when running Terraform
 		EnvVars: map[string]string{
 			"AWS_DEFAULT_REGION": awsRegion,
 		},
 	})
 
-	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
 
-	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform output` to get the value of an output variable
 	bucketID := terraform.Output(t, terraformOptions, "this_s3_bucket_id")
 	assert.Equal(t, expectedName, bucketID)
 
 	bucketRegion := terraform.Output(t, terraformOptions, "this_s3_bucket_region")
-	assert.Equal(t, awsRegion, bucketRegion)
+	assert.Equal(t, "us-east-1", bucketRegion)
 	
 }
