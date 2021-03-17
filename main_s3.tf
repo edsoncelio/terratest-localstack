42ae0b0213e6af0593b7b007585e8e5e1f38ae92
Edson (aka tuxpilgrim) <edsoncelio@users.noreply.github.com>
2021-03-17 09:59:48 -0300

    Update main_s3.tf

--- main_s3.tf
+++ main_s3.tf
@@ -8,17 +8,15 @@ terraform {
 }
 
 provider "aws" {
-  access_key                  = "mock_access_key"
-  region                      = "us-east-1"
+  region = "us-east-1"
   s3_force_path_style         = true
-  secret_key                  = "mock_secret_key"
+  secret_key                  = "test"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
   endpoints {
-    dynamodb       = "http://localhost:4569"
-    s3             = "http://localhost:4572"
+    s3             = "http://localstack:4566"
   }
 }
 
