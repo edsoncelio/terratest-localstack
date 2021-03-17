a396dd5af2ef5844e45a8e1df19b2d379f12a54d
Edson (aka tuxpilgrim) <edsoncelio@users.noreply.github.com>
2021-03-17 10:23:01 -0300

    Update aws_s3_helpers.go

--- test/aws_s3_helpers.go
+++ test/aws_s3_helpers.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 	"testing"
 
-	//"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
